5e98b831b28f75287d5ef3db2ce9c50d3ac7a438
pdrzewski <pedro.martins@stpech.school>
2026-09-02 11:10:35 -0300

    feat: adiciona arquivos base pro projeto

---
+++ src/main/java/ms/recupera_psswd/RecuperaPsswdApplication.java
@@ -0,0 +1,13 @@
+package ms.recupera_psswd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecuperaPsswdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecuperaPsswdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ms/recupera_psswd/RecuperaPsswdApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.recupera_psswd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecuperaPsswdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
