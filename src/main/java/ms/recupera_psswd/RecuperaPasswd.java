ad0114cdbd64ed7767fffa61819fcb514b3304da
pdrzewski <pedro.martins@stpech.school>
2026-09-02 11:20:23 -0300

    refactor: adiciona estrutura de pastas

--- src/main/java/ms/recupera_psswd/RecuperaPasswd.java
+++ src/main/java/ms/recupera_psswd/RecuperaPasswd.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RecuperaPsswdApplication {
+public class RecuperaPasswd {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RecuperaPsswdApplication.class, args);
+		SpringApplication.run(RecuperaPasswd.class, args);
 	}
 
 }
